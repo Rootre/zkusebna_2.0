cfc0913b35747fc129b467adcdf3fe732b5e33ed
Ondřej Tůma <ondrej@footshop.cz>
2017-12-29 19:35:30 +0100

    homepage first bite

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default () => <div>
+	<Head>
+		<title>{`Kobyliská zkušebna 2.0`}</title>
+		<meta charset={`UTF-8`}/>
+		<meta name={`viewport`} content={`initial-scale=1.0, width=device-width`} />
+	</Head>
+	<h1>Vítej, Zkušebno!</h1>
+</div>
